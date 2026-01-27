b47da8b803312380547023a3255f35fd4134cc99
Shashi kumar <shashi725806@gmail.com>
2026-01-27 17:12:14 +0530

    add a program sum, sub, mul, div

---
+++ basics/mix.cpp
@@ -0,0 +1,16 @@
+// Add, Subtract , Multiply, Divide
+ #include <iostream>
+ using namespace std;
+ int  main (){
+    
+    cout << " enter two number and find the sum, difference, multiple, division " << endl;
+    int a, b;
+cout << "Enter First Number ";
+cin >> a;
+cout << "Enter Second Number ";
+     cin >> b;
+     cout << "Sum of the number is " << a+b << "\n" << "difference of the number is " << a - b << "\n" << "multiple of the number is " << a * b << "\n" << "division of the number is " << a / b  ;
+
+    return 0;
+
+ }
