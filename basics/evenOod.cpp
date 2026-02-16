7176d74944cc703ebe2d6addf74af51a08da7314
Shashi kumar <shashi725806@gmail.com>
2026-02-16 23:17:04 +0530

    wap of even odd

---
+++ basics/evenOod.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int a;
+    cout << "Enter a number and check its even or odd ";  
+    cin >> a;     // user give the input
+    if ((a % 2) == 0)  // check the condition number is divisible by 2 or not 
+    cout << a << " is a even number";  
+    else
+     cout << a << " is a odd number";
+return 0;
+}
